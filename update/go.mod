f83b7f3615eb7520341491fd042cb32152df52fd
FactoryGIRL <65994850+Tohrusky@users.noreply.github.com>
2023-01-18 00:09:26 +0800

    Add files via upload

--- update/go.mod
+++ update/go.mod
@@ -3,6 +3,7 @@ module update
 go 1.19
 
 require (
+	github.com/gen2brain/go-unarr v0.1.6 // indirect
 	github.com/tidwall/gjson v1.14.4 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.1 // indirect
--- update/update.go
+++ update/update.go
@@ -3,10 +3,12 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"github.com/gen2brain/go-unarr"
 	"github.com/tidwall/gjson"
 	"io"
 	"net/http"
 	"os"
+	"path"
 )
 
 var GithubrpHost = "https://git.114514.lol/"           // 反代GitHub域名
@@ -15,8 +17,8 @@ var GithubAPIrpHost = "https://api-github.114514.lol/" // 反代GitHub API
 
 func getCurrentVersion() []string {
 	repositoryVset := "v1.0.0"
-	repositoryVsVsmlrt := "v1.0.0"
-	repositoryVsPytorch := "v1.0.0"
+	repositoryVsVsmlrt := "v1.0."
+	repositoryVsPytorch := "v1.0."
 	return []string{repositoryVset, repositoryVsVsmlrt, repositoryVsPytorch}
 }
 
@@ -51,16 +53,91 @@ func getDownloadLink(version []string) []string {
 	return []string{repositoryVset, repositoryVsVsmlrt, repositoryVsPytorch}
 }
 
-func downloadFile(url string, path string) (len int, err error) {
-	out, err := os.OpenFile("path/download", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-	defer out.Close()
+// 下载器结构体，实现io.Reader接口
+type MyDownloader struct {
+	io.Reader
+	TotalSize   int64
+	CurrentSize int64
+}
+
+// 实现Read方法
+func (d *MyDownloader) Read(p []byte) (n int, err error) {
+	n, err = d.Reader.Read(p)
+	d.CurrentSize += int64(n)
+	fmt.Printf("\r正在下载，下载进度：%.2f%%", float64(d.CurrentSize*10000/d.TotalSize)/100)
+	if d.CurrentSize == d.TotalSize {
+		fmt.Printf("\r下载完成，下载进度：%.2f%%", float64(d.CurrentSize*10000/d.TotalSize)/100)
+	}
+	return
+}
+
+func downloadFile(url string, p string) {
+	file, err := os.OpenFile(path.Join(p, "tempdl"), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
 	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
 	defer resp.Body.Close()
-	n, err := io.Copy(out, resp.Body)
-	return int(n), err
+
+	myDownloader := &MyDownloader{
+		Reader:    resp.Body,
+		TotalSize: resp.ContentLength,
+	}
+	if _, err := io.Copy(file, myDownloader); err != nil {
+		panic(err)
+	}
 }
 
 func main() {
-	//str, _ := os.Getwd()
-	fmt.Println(getLastestVersion())
+	MyDirPath, _ := os.Getwd()
+	CurrentVersion := getCurrentVersion()
+	LastestVersion := getLastestVersion()
+	DownloadLink := getDownloadLink(LastestVersion)
+
+	pgName := [][]string{{"VSET主程序", "VSET.zip"}, {"vs_vsmlrt环境包", "vs_vsmlrt.7z"}, {"vs_pytorch环境包", "vs_pytorch.7z"}}
+	cnt := 0
+	for i := 0; i < 3; i++ {
+		if LastestVersion[i] != CurrentVersion[i] {
+			cnt++
+
+			fmt.Println(pgName[i][0] + " 当前版本" + CurrentVersion[i] + "，" +
+				"有新版本" + LastestVersion[i] + "，按回车键开始下载")
+			_, scanln := fmt.Scanln()
+			if scanln != nil {
+				return
+			}
+
+			downloadFile(DownloadLink[i], MyDirPath)
+
+			_ = os.Rename(path.Join(MyDirPath, "tempdl"), path.Join(MyDirPath, pgName[i][1]))
+			DownloadFile, err := unarr.NewArchive(path.Join(MyDirPath, pgName[i][1]))
+			if err != nil {
+				panic(err)
+			}
+			_, err = DownloadFile.Extract(MyDirPath)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println("解压完成，要删除下载的压缩包吗？（y/n）")
+			var scan string
+			_, scanln = fmt.Scanln(&scan)
+			if scanln != nil {
+				return
+			}
+			if scan == "y" {
+				err = os.RemoveAll(path.Join(MyDirPath, pgName[i][1]))
+				if err != nil {
+					panic(err)
+				}
+			}
+			DownloadFile.Close()
+		}
+	}
+	if cnt == 0 {
+		fmt.Println("Ciallo~~恭喜你呀，当前已是最新版本")
+	}
 }
