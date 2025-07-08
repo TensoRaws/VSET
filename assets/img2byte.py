e6cf91fef4ad55804c3f4fcda4afe3e2f762a218
NangInShell <72263191+NangInShell@users.noreply.github.com>
2025-07-08 23:38:11 +0800

    Delete assets directory

--- assets/img2byte.py
+++
@@ -1,9 +0,0 @@
-import base64
-
-if __name__ == "__main__":
-    with open("logo.ico", "rb") as logo:
-        encoded_string = base64.b64encode(logo.read())
-        image_file = "logo_bytes = " + str(encoded_string)
-
-    with open("logo_bytes.py", "w") as logo_bytes:
-        logo_bytes.write(image_file)
