2a0592b20b37f7a575a2a7adf821a15d002d199d
Tohrusky <65994850+Tohrusky@users.noreply.github.com>
2025-08-17 15:06:07 +0800

    fix: del unused files

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -7,8 +7,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index'
 
-import './assets/main.css'
-
 import 'element-plus/dist/index.css'
 
 const app = createApp(App)
